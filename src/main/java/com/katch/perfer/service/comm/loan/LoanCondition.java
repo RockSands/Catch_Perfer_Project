01891f1017c742e86cebf87126538639abce598a
Administrator <Administrator@SC-201711102054>
2018-05-18 00:08:44 +0800

    等待测试

--- src/main/java/com/katch/perfer/service/comm/loan/LoanCondition.java
+++ src/main/java/com/katch/perfer/service/comm/loan/LoanCondition.java
@@ -1,16 +1,35 @@
 package com.katch.perfer.service.comm.loan;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.katch.perfer.mybatis.model.LoanConditionDefine;
 import com.katch.perfer.mybatis.model.TaxEnterpriseInfo;
 
 public abstract class LoanCondition {
-	
-	public abstract void init(LoanConditionDefine loanCondition);
-	
-	/**
-	 * 约束
-	 * @param info
-	 * @return
-	 */
-	public abstract boolean constraint(TaxEnterpriseInfo info);
+
+    @Autowired
+    private static List<LoanCondition> loanConditions;
+
+    public static LoanCondition getLoanCondition(LoanConditionDefine loanConditionDefine) {
+	LoanCondition loanCondition = null;
+	for (LoanCondition condeition : loanConditions) {
+	    loanCondition = condeition.init(loanConditionDefine);
+	    if (loanCondition != null) {
+		return loanCondition;
+	    }
+	}
+	return loanCondition;
+    }
+
+    public abstract LoanCondition init(LoanConditionDefine loanCondition);
+
+    /**
+     * 约束
+     * 
+     * @param info
+     * @return
+     */
+    public abstract boolean constraint(TaxEnterpriseInfo info);
 }
--- src/main/java/com/katch/perfer/service/comm/loan/condition/BusinessYears.java
+++ src/main/java/com/katch/perfer/service/comm/loan/condition/BusinessYears.java
@@ -1,5 +1,8 @@
 package com.katch.perfer.service.comm.loan.condition;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
@@ -12,22 +15,32 @@ import com.katch.perfer.service.comm.loan.LoanCondition;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 @Lazy
-public class BusinessDuration extends LoanCondition {
+public class BusinessYears extends LoanCondition {
+    
+    public BusinessYears() {
+	super();
+    }
 
-    private double value;
+    private int value;
 
     @Override
     public LoanCondition init(LoanConditionDefine loanCondition) {
-	if ("2".equals(loanCondition.getType())) {
-	    value = Double.valueOf(loanCondition.getVal());
-	    return this;
+	if ("1".equals(loanCondition.getType())) {
+	    value = Integer.valueOf(loanCondition.getVal());
 	}
 	return null;
     }
 
     @Override
     public boolean constraint(TaxEnterpriseInfo info) {
-	return info.getSnyye() > value;
+	if (info != null && info.getQsyysj() != null) {
+	    // 上海时间
+	    java.util.Calendar now = java.util.Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+	    java.util.Calendar start = java.util.Calendar.getInstance(TimeZone.getTimeZone("Asia/Shanghai"));
+	    start.setTime(info.getQsyysj());
+	    start.add(Calendar.YEAR, value);
+	    return start.before(now);
+	}
+	return true;
     }
-
 }
